0c00a380ce6864acf24980e8a4c4586221f00778
Isaac Martiniano <isaacam517@gmail.com>
2021-01-12 20:53:25 -0300

    Filmaria aplicação com vueJS consumindo api's com axios

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header id="header">
+      <router-link to="/" exact>Filmaria</router-link>
+      <router-link tag="button" to="/salvos" exact class="meusFilmes">SALVOS</router-link>
+
+  </header>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  #header{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    height: 60px;
+    background-color: crimson;
+    color: floralwhite;
+  }
+  #header a{
+    text-decoration: none;
+    font-size: 30px;
+    color: #FFF;
+    cursor: pointer;
+  }
+
+  button{
+    cursor: pointer;
+    border: 0;
+    background: #FFF;
+    padding: 8px 15px;
+    outline: none;
+    border-radius: 6px;
+    font-weight: bold;
+  }
+</style>
---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+// Api url: https://sujeitoprogramador.com/r-api/?api=filmes
+
+const api = axios.create({
+    baseURL: 'https://sujeitoprogramador.com/r-api'
+})
+export default api;
